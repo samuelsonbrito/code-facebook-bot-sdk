070de9badb288c69d25f696914e202c717234f64
Samuelson Brito <samuelsonma@hotmail.com>
2018-01-09 23:26:37 -0400

    templates criados

--- src/TemplatesMessage/ListTemplate.php
+++ src/TemplatesMessage/ListTemplate.php
@@ -8,6 +8,10 @@ class ReceiptTemplate implements TemplateInterface
 {
 	protected $products = [];
 	protected $recipientId;
+	protected $orderInfo;
+	protected $address;
+	protected $summary;
+	protected $adjustments;
 
     public function __construct(string $recipientId){
     	$this->recipientId = $recipientId;
@@ -16,28 +20,97 @@ class ReceiptTemplate implements TemplateInterface
     public function add(ElementInterface $element){
         $this->products[] = $element->get();
     }
+    
+    public function setOrderInfo(string $recipient_name, string $order_number, string $currency, string $payment_method, string $order_url, string $timestamp){
+        
+        $this->orderInfo = [
+            'recipient_name' => $recipient_name,
+            'order_number' => $order_number,
+            'currency' => $currency,
+            'payment_method' => $payment_method,
+            'order_url' => $order_url,
+            'timestamp' => $timestamp
+        ];
+        
+    }
+    
+    public function setAddress(string $street_1, string $street_2, string $city, string $postal_code, string $state, string $country){
+        
+        $this->address = [
+            'street_1' => $street_1,
+            'street_2' => $street_2,
+            'city' => $city,
+            'postal_code' => $postal_code,
+            'state' => $state,
+            'country' => $country
+        ];
+        
+    }
+    
+    public function setAdjustments(string $name, float $amount){
+        
+        $this->adjustments[] = [
+            'name' => $name,
+            'amount' => $amount
+        ];
+        
+    }
+    
+    public function setSummary(float $total_cost, float $subtotal = null, float $shopping_cost = null, float $total_tax = null){
+        
+        $this->summary = [
+            'total_cost' => $total_cost,
+            'total_cost' => $subtotal,
+            'total_cost' => $shopping_cost,
+            'total_cost' => $total_tax
+        ];
+        
+    }
 
 	public function message(string $messageText) :array{
+	    
+	    if($this->orderInfo !== null){
+	        throw new \Exception('order is required');
+	    }
+	    
+	    if($this->summary !== null){
+	        throw  new \Exception('summary is required');
+	    }
 
-		return [
+		$result = [
 
 			'recipient' => [
-
 				'id' => $this->recipientId
-
 			],
-
     		'message' => [
     			'attachment' => [
     				'type' => 'template',
     				'payload' => [
-    					'template_type' => 'list',
+    					'template_type' => 'receipt',
     					'text' => $messageText,
-    					'buttons' => $this->products
+    				    'recipient_name' => $this->orderInfo['recipient_name'],
+    				    'order_number' => $this->orderInfo['$order_number'],
+    				    'currency' => $this->orderInfo['currency'],
+    				    'payment_method' => $this->orderInfo['payment_method'],
+    				    'order_url' => $this->orderInfo['order_url'],
+    				    'timestamp' => $this->orderInfo['timestamp'],
+    				    'elements' => $this->products,
+    				    'summary' => $this->summary
     				]
     			]
     		]
 
 		];
+		
+		if($this->address !== null){
+		    $result['message']['attachment']['payload']['address'] = $this->address;
+		}
+		
+		if($this->adjustments !== null){
+		    $result['message']['attachment']['payload']['adjustments'] = $this->adjustments;
+		}
+		
+		return $result;
+		
 	}
 }
