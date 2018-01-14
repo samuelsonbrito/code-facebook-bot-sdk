2d99a3079d0481fb7568ad87e7f0d71474837915
Samuelson Brito <samuelsonma@hotmail.com>
2018-01-13 21:12:57 -0400

    templates finalizados

--- tests/integration/TemplatesMessage/ListTemplateTest.php
+++ tests/integration/TemplatesMessage/ListTemplateTest.php
@@ -1,13 +1,54 @@
 <?php
 namespace CodeBot\TemplatesMessage;
 
+use CodeBot\Element\Button;
+use CodeBot\Element\Product;
 use PHPUnit\Framework\TestCase;
 
 class ListTemplateTest extends TestCase
 {
 
-    public function testAs(){
-        $this->assertEquals(true, true);
+    public function testListaComTresProdutos() {
+        
+        $product = new Product('Produto','https://vuejs.org/images/logo.png','Curso de VueJS', new Button('web_url',null,'https://vuejs.org'));
+        
+        $template = new ListTemplate(1234);
+        $template->add($product);
+        
+        $actual = $template->message('qwe');
+        
+        $expected = [
+            
+            'recipient' => [
+                
+                'id' => 1234
+                
+            ],
+            
+            'message' => [
+                'attachment' => [
+                    'type' => 'template',
+                    'payload' => [
+                        'template_type' => 'list',
+                        'buttons' => [
+                            [
+                                'title' => 'Produto',
+                                'subtitle' => 'Curso de VueJS',
+                                'image_url' => 'https://vuejs.org/images/logo.png',
+                                'default_action' => [
+                                    'type' => 'web_url',
+                                    'url' => 'https://vuejs.org'
+                                ],
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+            
+        ];
+        
+        $this->assertEquals($expected, $actual);
+        
     }
 }
 
