59817e7767d9acfc67c97fda7094f86269d78eb1
Samuelson Brito <samuelsonma@hotmail.com>
2018-01-15 19:31:33 -0400

    remoção do botão

--- tests/integration/GetStartedButtonTest.php
+++ tests/integration/GetStartedButtonTest.php
@@ -9,8 +9,17 @@ class GetStartedButtonTest extends TestCase
     public function testAddGetStartedButton()
     {
         $data = (new GetStartedButton())->add('iniciar');
-        $callSendApi = new CallSendApi('aqui vai seu page access token');
+        $callSendApi = new CallSendApi('TOKEN');
         $result = $callSendApi->make($data, CallSendApi::URL_PROFILE);
+        
+        $this->assertTrue(is_string($result));
+    }
+    
+    public function testRemoveGetStartedButton()
+    {
+        $data = (new GetStartedButton())->remove();
+        $callSendApi = new CallSendApi('TOKEN');
+        $result = $callSendApi->make($data, CallSendApi::URL_PROFILE, 'DELETE');
         
         $this->assertTrue(is_string($result));
     }
